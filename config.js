4f3d299bba7507dcec6c763b91136379af08ca42
usernoobiee <97098845+usernoobiee@users.noreply.github.com>
2026-02-14 15:59:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓' ,'💞', '💕'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['❤️', '💖', '💝', '💗', '💓' ,'💞', '💕', '❤️', '💖', '💝', '💗', '💓' ,'💞', '💕'],  // Heart emojis
+        bears: ['🧸', '🐻', '🧸', '🐻', '🧸', '🐻']                       // Cute bear emojis
     },
 
     // Questions and answers
